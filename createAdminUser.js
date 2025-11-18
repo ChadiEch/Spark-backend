b29f4cd5c26238afcf944c8ee48abe85e84d5da1
ChadiEch <chadiech18@gmail.com>
2025-11-18 15:31:35 +0200

    fix(env): change NODE_ENV from development to production

--- createAdminUser.js
+++ createAdminUser.js
@@ -13,7 +13,7 @@ const User = require('./models/User');
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-});
+}); 
 
 const createAdminUser = async () => {
   try {
